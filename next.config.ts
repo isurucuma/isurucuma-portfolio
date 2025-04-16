0f69644e6d9bee1d0a43d66a7c630bc1affe1fa9
isuru <isurucuma@gmail.com>
2025-04-16 19:13:38 +0530

    base path configured

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
